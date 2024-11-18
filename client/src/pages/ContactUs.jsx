6b41dd8d7323614e881d085625973253e33d74af
Rajdiwate <127538240+Rajdiwate@users.noreply.github.com>
2024-11-18 17:54:58 +0530

    change policy

--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const ContactUs = () => {
   return (
-    <div>Contact us via email at rajdiwate786@gmail.com or call 9325682482. Visit our clinic . Follow us on social media for updates.</div>
+    <div>Contact Us
+    Last updated on 18-11-2024 17:52:59
+    You may contact us using the information below:
+    Merchant Legal entity name: RAJ AJAY DIWATE
+    Registered Address: Gandhi Nagar, Pulgaon, Maharashtra, PIN: 442302
+    Operational Address: Gandhi Nagar, Pulgaon, Maharashtra, PIN: 442302
+    Telephone No: 9325682482
+    E-Mail ID: rajdiwate786@gmail.com</div>
   )
 }
 
